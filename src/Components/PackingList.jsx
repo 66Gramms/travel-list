f7d8b0b3bb8c8638e48073b4d49bfc3dd91c3d0a
66Gramms <meszarosricsi01@gmail.com>
2024-01-20 21:39:52 +0100

    Organize components into folders

---
+++ src/Components/PackingList.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Item from "./App";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  switch (sortBy) {
+    case "input":
+      sortedItems = items;
+      break;
+    case "packed":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => Number(a.packed) - Number(b.packed));
+      break;
+    case "description":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => a.description.localeCompare(b.description));
+      break;
+  }
+  console.log(items);
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="packed">Sort by status</option>
+          <option value="description">Sort alphabetically</option>
+        </select>
+        <button onClick={() => onClearList()}>Clear list</button>
+      </div>
+    </div>
+  );
+}
